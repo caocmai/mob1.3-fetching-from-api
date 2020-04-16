bfdb2b79a840e6a9e137583e854d72d8a31c6d4b
Cao Mai <caomai@rocketmail.com>
2020-04-15 22:37:12 -0700

    first commit

---
+++ DailyPlanet/DailyPlanet/PokiCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokiCell.swift
+//  DailyPlanet
+//
+//  Created by Cao Mai on 4/15/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PokiCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
